9a2c1518ab807b389400584a1524bf0f86b0d3d0
HONOR <busyginz@mail.ru>
2024-11-15 14:08:43 +0700

    views 15/11/2024

--- stepic/blog/urls.py
+++ stepic/blog/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
-from .views import homepageview
+from django.urls import path, re_path
+from blog import views
 
-urlpatterns = [path("", homepageview, name='home'), ]
+urlpatterns = [
+               path("", views.index, name='home'),
+               path("about/", views.about, kwargs={"name": "Tom", "age": 38}),
+               re_path(r"^contact/", views.contact),
+                
+               ]
--- stepic/blog/views.py
+++ stepic/blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def homepageview(request):
-    return HttpResponse("Hello world!")
+def index(request):
+    return HttpResponse('<h2>Главная</h2>')
+
+def about(request, name, age):
+    return HttpResponse(f'''
+                    <h2>О пользователе</h2>
+                    <p>Имя: {name}</p>
+                    <p>Возраст: {age}</p>
+''')
+
+def contact(request):
+    return HttpResponse('<h2>Контакты</h2>')
+
