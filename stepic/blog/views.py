406b2d774d2fa9eb9699ec2a19a9c4a6fe3077db
Yugenyi Busygin <abusyginza@gmail.com>
2024-12-07 16:26:55 +0700

    2-2.6-4 for

--- stepic/blog/views.py
+++ stepic/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
  
-def index(request):
-    data = {"age": 70}
-    return render(request, "blog/index.html", context=data)
 
+def index(request):
+    cat = ["Python", "Java", "JS", "GO", "C#", "Kotlin"]
+    return render(request, "blog/index.html", context={"cat":cat})
 
