198fcf2205e2f38a6bc8717b13e820855fbcddd6
Yugenyi Busygin <abusyginza@gmail.com>
2024-12-07 16:18:53 +0700

    2-2.6-4  if enif

--- stepic/blog/views.py
+++ stepic/blog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
  
 def index(request):
-    header = "Данные пользователя"
-    langs = ['Python', "Java", "C#"]
-    user = {"name": "Tom", "age": 23}
-    address = ("Абрикосовая", 23, 45)
-    
-    data = {'header': header, "langs": langs, "user": user, "adress": address}
+    data = {"age": 70}
     return render(request, "blog/index.html", context=data)
+
 
